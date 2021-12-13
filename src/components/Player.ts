d35c4df2ce1df085e5b7c130e70a1dee0f2209d0
jakubbajer <kuba@bajer.net>
2021-12-13 11:01:21 +0100

    Added runing only with shift pressed

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -1,14 +1,14 @@
 // Game physics info
-// min walk velocity - 00130
-// max walk velocity - 01900
-// walking acceleration - 00098
-// running acceleration - 000E4
-// max running speed - 02900
-// release deceleration - 000D0
-// skidding deceleration - 0001A
-// max walk velocity (underewater) - 01100
-// max walk velocity (level entry) - 00D00
-// skid turnaround speed - 00900
+// min walk velocity - 0x00130
+// max walk velocity - 0x01900
+// walking acceleration - 0x00098
+// running acceleration - 0x000E4
+// max running speed - 0x02900
+// release deceleration - 0x000D0
+// skidding deceleration - 0x0001A
+// max walk velocity (underewater) - 0x01100
+// max walk velocity (level entry) - 0x00D00
+// skid turnaround speed - 0x00900
 
 import world1_1 from "../../assets/sprites/world1-1-bg.png";
 import Camera from "./Camera";
@@ -16,8 +16,6 @@ import Controls from "./Controls";
 
 class Player {
   positionX: number;
-  maxPositionX: number;
-  minPositionX: number;
   velocityX: number;
   positionY: number;
   velocityY: number;
@@ -26,9 +24,7 @@ class Player {
   controls: Controls;
 
   constructor(context: CanvasRenderingContext2D) {
-    this.positionX = 0x0;
-    this.maxPositionX = 0x0;
-    this.minPositionX = 0x0;
+    this.positionX = 0x03000 / 0x100;
     this.velocityX = 0x0;
     this.positionY = 0x0;
     this.velocityY = 0x0;
@@ -48,8 +44,6 @@ class Player {
     if (this.positionX < this.camera.position)
       this.positionX = this.camera.position;
 
-    console.log();
-
     this.draw(this.positionX - this.camera.position, this.positionY);
   }
 
@@ -60,22 +54,44 @@ class Player {
     this.context.stroke();
   }
 
-  // TODO: make accelerating with shift running
   accelerate(right: boolean, run: boolean) {
-    if (right) {
-      if (this.velocityX < 0x00130 / 0x100)
-        this.velocityX = 0x00130 / 0x100;
-      else
-        this.velocityX += 0x00098 / 0x100;
-      if (this.velocityX >= 0x00190 / 0x100)
-        this.velocityX = 0x00190 / 0x100;
-    } else {
-      if (this.velocityX > -0x00130 / 0x100)
-        this.velocityX = -0x00130 / 0x100;
-      else
-        this.velocityX -= 0x00098 / 0x100;
-      if (this.velocityX <= -0x00190 / 0x100)
-        this.velocityX = -0x00190 / 0x100;
+    if (run) {
+      if (right) {
+        // running right
+        if (this.velocityX < 0x00130 / 0x100)
+          this.velocityX = 0x00130 / 0x100;
+        else
+          this.velocityX += 0x000E4 / 0x100;
+        if (this.velocityX >= 0x00290 / 0x100)
+          this.velocityX = 0x00290 / 0x100;
+      } else {
+        // running left
+        if (this.velocityX > -0x00130 / 0x100)
+          this.velocityX = -0x00130 / 0x100;
+        else
+          this.velocityX -= 0x000E4 / 0x100;
+        if (this.velocityX <= -0x00290 / 0x100)
+          this.velocityX = -0x00290 / 0x100;
+      }
+    }
+    else {
+      if (right) {
+        // walking right
+        if (this.velocityX < 0x00130 / 0x100)
+          this.velocityX = 0x00130 / 0x100;
+        else
+          this.velocityX += 0x00098 / 0x100;
+        if (this.velocityX >= 0x00190 / 0x100)
+          this.velocityX = 0x00190 / 0x100;
+      } else {
+        // walking left
+        if (this.velocityX > -0x00130 / 0x100)
+          this.velocityX = -0x00130 / 0x100;
+        else
+          this.velocityX -= 0x00098 / 0x100;
+        if (this.velocityX <= -0x00190 / 0x100)
+          this.velocityX = -0x00190 / 0x100;
+      }
     }
   }
 
